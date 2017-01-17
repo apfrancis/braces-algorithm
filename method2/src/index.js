6202c59c50fa43385179f19d50917b1dff66a63d
Andrew Francis <apfrancis@gmail.com>
2017-01-17 23:54:37 +0000

    tests and code for an isBalanced function. There are some outstanding edge cases where this doesn't work.

--- method2/src/index.js
+++ method2/src/index.js
@@ -7,9 +7,23 @@ const isClosingBrace = char => closingBraces.indexOf(char) != -1
 const isCorrespondingBrace = (openingBrace, closingBrace) =>
     openingBraces.indexOf(openingBrace) == closingBraces.indexOf(closingBrace)
 
+const isBalanced = (input, stack) => {
+    if (input === '') {
+        return (stack === '')
+    } else if (isOpeningBrace(input.charAt(0))) {
+        return isBalanced(input.substr(1), input.charAt(0) + stack)
+    } else if (isClosingBrace(input.charAt(0))) {
+        return !(stack === '')
+            && isCorrespondingBrace(stack.charAt(0), input.charAt(0))
+            && isBalanced(input.substr(1), stack.substr(1))
+    } else {
+        isBalanced(input.substr(1),stack)
+    }
+}
 
 module.exports = {
-    isOpeningBrace:isOpeningBrace,
-    isClosingBrace:isClosingBrace,
-    isCorrespondingBrace:isCorrespondingBrace
+    isOpeningBrace,
+    isClosingBrace,
+    isCorrespondingBrace,
+    isBalanced
 }
--- method2/test/index.spec.js
+++ method2/test/index.spec.js
@@ -1,7 +1,7 @@
 const test = require('tape')
 const braces = require('../src/index')
 
-test('should identify opening braces', function (t) {
+test('should identify opening braces', t => {
   const vals = [
       '{',
       '[',
@@ -13,7 +13,7 @@ test('should identify opening braces', function (t) {
   t.end()
 })
 
-test('should identify closing braces', function (t) {
+test('should identify closing braces', t => {
     const vals = [
         '}',
         ']',
@@ -25,14 +25,36 @@ test('should identify closing braces', function (t) {
     t.end()
 })
 
-test('should identify matching pairs of braces', function (t) {
+test('should identify matching pairs of braces', t => {
     t.true(braces.isCorrespondingBrace('(', ')'))
     t.true(braces.isCorrespondingBrace('{', '}'))
     t.true(braces.isCorrespondingBrace('[', ']'))
     t.end()
 })
 
-test('should return false if braces dont match', function (t) {
+test('should return false if braces dont match', t => {
     t.false(braces.isCorrespondingBrace('(', '}'))
+    t.end()
+})
+
+test('should balance braces', t => {
+    const vals = [
+        '()',
+        '{()[]}'
+    ]
+    vals.map(function(val){
+        t.true(braces.isBalanced(val,''))
+    })
+    t.end()
+})
+
+test('should report if braces are imbalanced', t => {
+    const vals = [
+        '((',
+        '{({{}'
+    ]
+    vals.map(function(val){
+        t.false(braces.isBalanced(val,''))
+    })
     t.end()
 })
